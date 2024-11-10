98daaa72f26461eede9db1942a629ad5dca83667
Mazino <fredozv01@gmail.com>
2024-11-10 16:56:42 +0100

    resultats en float

--- tuto.js
+++ tuto.js
@@ -1,21 +1,21 @@
 
-function calculateurIMc (poids ,taille)
+function calculeIMC (poids ,taille)
 {
-let poidsKg = poids/1000; 
-tailleCarre = taille*taille ;
-let result = poidsKg/tailleCarre ;
+tailleEnMetres = taille/100 ;  //conversion taille en metre
+tailleCarre = Math.pow(tailleEnMetres,2); //taille au carré 
+let result = poids/tailleCarre ;
 return result ;
 
 };
 
 
-let poids = prompt('Quelle est ton poids en g ?');
-poids = Number(poids); //conversion string en int 
+let poids = prompt('Quelle est ton poids en kg ?');
+//poids = Number(poids); //conversion string en int 
 
 let taille = prompt('Quelle est ton taille en cm ?');
-taille = Number(taille); //conversion string en int 
-tailleEnMetre = taille/100 ;  //conversion taille en metre
+//taille = Number(taille); //conversion string en int 
 
-result = calculateurIMc (poids ,tailleEnMetre);
+
+result = calculeIMC (poids ,taille);
 result = parseFloat(result.toFixed (2) );  //conversion int en float
 alert ('Le resulta de votre IMC = '+ result);
